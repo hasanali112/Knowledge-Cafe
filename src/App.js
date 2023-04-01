4572a751757200e46115b747a8a99af67d309bfb
hasanali112 <mdhasan.ru.ro@gmail.com>
2023-04-01 22:17:27 +0600

    card image and blog detail add

--- src/App.js
+++ src/App.js
@@ -36,11 +36,14 @@ function LoadCardData() {
   },[])
 
   return (
-    <div className="card-detail">
-      <div className="card-component">
-          <div className="card-info">
+    <div>
+      <div>
+          <div>
              {
-              cardInfos.map(cardInfo=> console.log(cardInfo))
+              cardInfos.map(cardInfo=> <CardInfo 
+              cardInfo={cardInfo}
+              >  
+              </CardInfo>)
              }
         </div>
       <div>
@@ -51,5 +54,22 @@ function LoadCardData() {
   );
 }
 
-
+function CardInfo (props){
+  const {img, Publish_Date, read_time, blog_title, authorname }= props.cardInfo;
+  return (
+      <div className='card-detail'>
+        <div className="card-info">
+            <img className="card-info-img" src={img} alt="" />
+             <div className="card-subinfo">
+                 <img className="card-subinfo-img" src={logo} alt="" /> 
+                   <div className="card-subinfo-title-body">
+                        <h6 className="card-subinfo-titile1">{authorname}</h6>
+                        <h5 className="card-subinfo-titile2">{Publish_Date}</h5> 
+                    </div>
+             </div>
+             <p className="blog-title">{blog_title} stars</p>
+        </div>
+      </div>
+  );
+};
 export default App;
